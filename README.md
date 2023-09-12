1c70f0d0391a847df0be373374b77609de3347d9
Kevin Manurung <kevinmanurung0207@gmail.com>
2023-09-12 23:57:36 +0700

    menambahkan model

---
+++ README.md
@@ -0,0 +1,6 @@
+
+LINK REPO : https://github.com/KayeyeY/tugas2.git
+
+
+Jelaskan bagaimana cara kamu mengimplementasikan checklist di atas secara step-by-step (bukan hanya sekadar mengikuti tutorial).
+- 
--- main/models.py
+++ main/models.py
@@ -6,3 +6,4 @@ class Product(models.Model):
     date_added = models.DateField(auto_now_add=True)
     price = models.IntegerField()
     description = models.TextField()
+    amount = models.IntegerField()
